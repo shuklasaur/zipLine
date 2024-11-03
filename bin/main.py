9c82572f61f78d4c46e494797fcd40b62bcd900b
shuklasaur <ankitshukla2024@gmail.com>
2024-11-03 13:11:37 +0530

    added functionality to read data in pandas

--- bin/main.py
+++ bin/main.py
@@ -1,1 +1,29 @@
-print('Hello zipLine')
+import glob
+import os
+import hashlib
+import pandas as pd
+
+directory = "/mnt/devdrive"
+
+files_and_dirs = glob.glob(directory + "/**/*.*", recursive=True)
+files = [file for file in files_and_dirs if os.path.isfile(file)]
+hashSet = set()
+file_hash_dict = {}
+
+
+i = 0
+
+for file in files:
+    i = i + 1
+    # print(file)
+    # print(open(file,'rb').read(1024*1024))
+    hashSet.add(hashlib.sha256(open(file, "rb").read(1024 * 1024)).hexdigest())
+    file_hash_dict[file] = (hashlib.sha256(open(file, "rb").read(1024 * 1024)).hexdigest())
+
+
+df = pd.DataFrame({"filename": file_hash_dict.keys(), "hash": file_hash_dict.values()})
+
+# print(df.hash.unique())
+print(df)r
+
+# print(file_hash_dict)
