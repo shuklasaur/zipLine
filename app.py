12454590e2818dfe51392d270006285824eb6cbb
saurabhshukladev <saurabhshukla9712@gmail.com>
2025-02-03 03:53:13 +0000

    syncing

--- app.py
+++ app.py
@@ -37,3 +37,13 @@ def postimage():
     flask.request.files.get("image").save(f"testData/new-{flask.request.files.get('image').filename}")
 
     return 'GOOD'
+
+@app.route("/post/images",methods=["POST"])
+def postimages():
+    print(flask.request.content_type)
+    data = flask.request.files["image1"]
+    print(data)
+
+    with open("testData/op/op1.jpeg","wb") as f:
+        f.write(data.read())
+    return 'Hello from the children of planet Earth.'
